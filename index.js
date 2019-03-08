128c995a704a452ca6c730ca124c3e69acef93ae
Steven Enamakel <enamakel@cryptocontrol.io>
2019-03-08 14:44:00 +0530

    added check for error message

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ process.env.ROOT_PATH = __dirname
 
 
 if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'test') {
+  if (!process.env.SERVER_SECRET || process.env.SERVER_SECRET === 'secret_keyboard_cat') {
+    console.error('you need to set the SERVER_SECRET environment variable to something secretive')
+    return process.exit(1)
+  }
+
   require('./dist')
   return
 }
