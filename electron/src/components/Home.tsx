3c86c81b713c82a03b826630c5ec71420e3abcb2
Steven Enamakel <enamakel@cryptocontrol.io>
2019-03-13 21:10:24 +0530

    cleaned up the UI

--- electron/src/components/Home.tsx
+++ electron/src/components/Home.tsx
@@ -2,10 +2,12 @@ import * as React from 'react'
 import server from '../../../src/server'
 import './style.sass'
 
+
 interface IState {
   error?: string
   loading: boolean
   messages: string[]
+  password: string
   isServerRunning: boolean
 }
 
@@ -16,6 +18,7 @@ export default class Home extends React.Component<{}, IState> {
 
   state: IState = {
     loading: false,
+    password: '',
     messages: [],
     isServerRunning: false
   }
@@ -28,10 +31,11 @@ export default class Home extends React.Component<{}, IState> {
       const port = process.env.PORT || 8080
       this.addMessage('starting server on port ' + port)
 
-      this.instance = server.listen(port, () => {
-        this.addMessage('listening on port ' + port)
-        this.setState({ isServerRunning: true, loading: false })
-      })
+      process.env.SERVER_SECRET = this.state.password
+      this.instance = server.listen(port)
+
+      this.addMessage('listening on port ' + port)
+      this.setState({ isServerRunning: true, loading: false })
     } catch (e) {
       this.addMessage('Error! ' + e.message)
       this.setState({ error: e.message, loading: false })
@@ -59,23 +63,50 @@ export default class Home extends React.Component<{}, IState> {
   }
 
 
+  renderConnectInstructions () {
+    if (!this.state.isServerRunning) return
+    return (
+      <div>
+        <p>
+          Great! the server is running. In the CryptoControl terminal enter in
+          the following details
+        </p>
+
+        <div className="connect-inst">
+          Server Url: <b>http://127.0.0.1:8080</b>
+        </div>
+        <div className="connect-inst">
+          Password: <b>{process.env.SERVER_SECRET}</b>
+        </div>
+      </div>
+    )
+  }
+
   render() {
-    const { isServerRunning, loading } = this.state
+    const { isServerRunning, loading, password } = this.state
 
     return (
       <div id="page-home">
         <h1>CryptoControl Trading Server</h1>
-        <p>
-          To start the server, please enter a password and
-          then press the start button.
-        </p>
+        <header>
+          <p>
+            This server is meant to be used by the CryptoControl Terminal to execute advanced orders like stop-losses, trailing stop-losses, take profit (and more) on exchanges that don't support advanced orders.
+          </p>
+          <p>
+            To start the server, please enter a password and
+            then press the start button.
+          </p>
+        </header>
 
         <div className="divider" />
 
         <div>
           <div className="password-section">
             <div>
-              Password: <input type="text" placeholder="something secretive" />
+              Password (required): <input
+                onChange={e => this.setState({ password: e.target.value })}
+                type="text"
+                placeholder="something secretive" />
             </div>
             <p>
               The password is used to encrypt all your API keys and is used to authenticate
@@ -86,16 +117,18 @@ export default class Home extends React.Component<{}, IState> {
           <div>
             <button
               onClick={this.runServer}
-              disabled={isServerRunning || loading} >
+              disabled={isServerRunning || loading || password.length < 2} >
               Start Server
             </button>
 
-            <button
+            {/* <button
               onClick={this.closeServer}
               disabled={!isServerRunning || loading} >
                 Stop Server
-            </button>
+            </button> */}
           </div>
+
+          {this.renderConnectInstructions()}
 
           <div>Server Status: &nbsp;
             {isServerRunning ?
@@ -105,20 +138,23 @@ export default class Home extends React.Component<{}, IState> {
         </div>
 
         <div className="divider" />
-        <p>For any issues, please feel free to contact us at contact@cryptocontrol.io</p>
-        <p>
-          Source code available
-          at <a target="_blank" href="https://github.com/cryptocontrol/algo-trading-server">github.com/cryptocontrol/algo-trading-server</a>
-        </p>
+        <footer>
+          <p>
+            For any issues, please feel free to contact us
+            at <a target="_blank" href="mailto:contact@cryptocontrol.io">contact@cryptocontrol.io</a>.
+            Source code available
+            at <a target="_blank" href="https://github.com/cryptocontrol/algo-trading-server">github.com/cryptocontrol/algo-trading-server</a>
+          </p>
+        </footer>
 
-        <div className="divider" />
+        {/* <div className="divider" /> */}
 
-        <div className="messages">
+        {/* <div className="messages">
           <b> Message Console: </b>
           {this.state.messages.map((message, index) => {
             return (<div key={index} className="message">{message}</div>)
           })}
-        </div>
+        </div> */}
       </div>
     );
   }
