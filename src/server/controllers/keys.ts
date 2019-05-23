63ef9bc1b289d878001e20d7c38da5e58244190c
Steven Enamakel <enamakel@cryptocontrol.io>
2019-05-23 12:40:23 +0530

    added check to see if the API key is valid or not

--- src/server/controllers/keys.ts
+++ src/server/controllers/keys.ts
@@ -1,4 +1,6 @@
+import * as ccxt from 'ccxt'
 import * as _ from 'underscore'
+
 
 import UserExchanges from 'src/database/models/userexchanges'
 
@@ -7,6 +9,19 @@ import UserExchanges from 'src/database/models/userexchanges'
  * Set the API key for an exchange for the logged in user
  */
 export const setAPIKey = async (uid: string, exchange: string, data: any) => {
+  // first authenticate with the exchange and see if the API key works
+  const ccxtExchange = new ccxt[exchange]({
+    apiKey: data.key,
+    secret: data.secret,
+    password: data.password,
+    useServerTime: true,
+    enableRateLimit: true
+  })
+
+  // Make a sample request to fetch the user's balance; If this fn returns successfully
+  // then we save the API keys and continue
+  await ccxtExchange.fetchBalance()
+
   // find or update logic
   const found = await UserExchanges.findOne({ where: { uid, exchange } })
 
