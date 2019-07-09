cb1ffef556848d55cd8ddb59230c94533dea2d72
ditpoo <ditpoo@gmail.com>
2019-07-09 15:45:26 +0530

    Add changes to cancel order

--- src/triggers/CancelOrderTrigger.ts
+++ src/triggers/CancelOrderTrigger.ts
@@ -7,8 +7,6 @@ import { ICandle } from "src/interfaces";
 // TODO: Write code for cancel order trigger
 
 export default class CancelOrderTrigger extends BaseTrigger {
-  orderId: number;
-  condition: number;
   action: 'cancel'
 
 
@@ -16,8 +14,6 @@ export default class CancelOrderTrigger extends BaseTrigger {
     super(trigger, "Cancel Order")
 
     const params = JSON.parse(trigger.params);
-    this.orderId = params.orderId;
-    this.condition = params.condition;
     this.action = params.action;
 
     if (params.action !== 'cancel') throw new Error('bad/missing action');
@@ -27,7 +23,7 @@ export default class CancelOrderTrigger extends BaseTrigger {
     if (!this.isLive) return;
     const { price } = trade;
 
-    if (price <= this.condition) {
+    if (price <= this.triggerDB.targetPrice) {
       this.advice("cancel-order", price, this.triggerDB.targetVolume);
       this.close();
     }
