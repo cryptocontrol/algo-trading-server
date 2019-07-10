9316b53050d5eb599d33223f2da66b150eab3015
ditpoo <ditpoo@gmail.com>
2019-07-10 12:26:10 +0530

    Add condition parmeter to cancel order tigger

--- src/triggers/CancelOrderTrigger.ts
+++ src/triggers/CancelOrderTrigger.ts
@@ -8,6 +8,7 @@ import { ICandle } from "src/interfaces";
 
 export default class CancelOrderTrigger extends BaseTrigger {
   action: 'cancel'
+  condition: [string];
 
 
   constructor(trigger: Triggers) {
@@ -15,15 +16,27 @@ export default class CancelOrderTrigger extends BaseTrigger {
 
     const params = JSON.parse(trigger.params);
     this.action = params.action;
+    this.condition = params.condition;
+
 
     if (params.action !== 'cancel') throw new Error('bad/missing action');
+
+    if ([
+      'greater-than',
+      'greater-than-equal',
+      'less-than',
+      'less-than-equal'
+    ].indexOf(params.condition) === -1) throw new Error('bad/missing condition');
   }
 
   onTrade(trade: Trade) {
     if (!this.isLive) return;
     const { price } = trade;
 
-    if (price <= this.triggerDB.targetPrice) {
+    if (price <= this.triggerDB.targetPrice && this.condition.toString() === "less-than-equal"
+      || price < this.triggerDB.targetPrice && this.condition.toString() === "less-than"
+      || price >= this.triggerDB.targetPrice && this.condition.toString() === "greater-than-equal"
+      || price > this.triggerDB.targetPrice && this.condition.toString() === "greater-than") {
       this.advice("cancel-order", price, this.triggerDB.targetVolume);
       this.close();
     }
