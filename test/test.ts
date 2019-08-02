11077482b7c92a3395eb7cfcfcbc326a895ed9c9
ditpoo <ditpoo@gmail.com>
2019-08-02 16:09:45 +0530

    Add dynamci tiered take profit

--- test/test.ts
+++ test/test.ts
@@ -2,10 +2,12 @@ import stopLossTriggerTests from "./Trigger/stopLossTriggerTests";
 import takeProfitTriggerTests from "./Trigger/takeProfitTriggerTests";
 import cancelOrderTriggerTests from "./Trigger/cancelOrderTriggerTests";
 import tieredTakeProfitTriggerTests from "./Trigger/tieredTakeProfitTriggerTests";
-import dynamicTieredTakeProfitTriggerTests from "./Trigger/dynamicTieredTakeProfitTriggerTests";
+// import dynamicTieredTakeProfitTriggerTests from "./Trigger/dynamicTieredTakeProfitTriggerTests";
+import stopLossTakeProfitTriggerTests from "./Trigger/stopLossTakeProfitTests";
 
 stopLossTriggerTests;
 takeProfitTriggerTests;
 cancelOrderTriggerTests;
 tieredTakeProfitTriggerTests;
 // dynamicTieredTakeProfitTriggerTests;
+stopLossTakeProfitTriggerTests;
