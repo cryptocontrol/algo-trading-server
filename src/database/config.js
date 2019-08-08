4b066959f06b1bb0ca3972bb721389332816ee2d
Steven Enamakel <enamakel@cryptocontrol.io>
2019-08-08 15:11:46 +0530

    use sqlite for production

--- src/database/config.js
+++ src/database/config.js
@@ -15,10 +15,7 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'cctrader_prod',
-    host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
   }
 }
